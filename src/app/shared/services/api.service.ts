66fd9ea1955fdb991117a44139b413b7b35c6a19
Filip Weilid <filip.weilid@gmail.com>
2018-12-19 13:53:31 +0100

    import

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  get<T>(url: string, opts = {}): Observable<T> {
+    return this.http.get<T>(url, opts).pipe(
+      map(data => data)
+    )
+  }
 }
