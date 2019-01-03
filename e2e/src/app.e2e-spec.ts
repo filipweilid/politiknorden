90b599cf45bd954ff0ee4537b5a87dce2377881f
malinklang <malin-klang@hotmail.com>
2019-01-03 12:18:31 +0100

    test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to Politik!');
+    expect(page.getTitleText()).toEqual('Welcome to Politiik!');
   });
 });
