90608350389dd019750fa7961650d9100ebda153
filipweilid <filip.weilid@gmail.com>
2019-01-13 23:13:03 +0100

    Server för twitter och api calls till twitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './shared/services/api.service';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+export interface TwitterApi {
+    data: {
+        statuses: any
+    }
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  data$: any;
+    data$: any;
+    searchquery = 'Ulf Kristersson';
+    tweetsdata;
 
-  constructor(private http: ApiService) { }
+    constructor(private http: ApiService, private http_: HttpClient) { }
 
-  ngOnInit() {
+    ngOnInit() {
+        this.searchcall();
+    }
 
-  }
+    makecall() {
+        var headers = new HttpHeaders();
 
-  title = 'Politiknördarna';
+        headers.append('Content-Type', 'application/X-www-form-urlencoded');
+
+        this.http_.post('http://localhost:3000/authorize', { headers: headers }).subscribe((res) => {
+            console.log(res);
+        })
+    }
+
+    searchcall() {
+        var headers = new HttpHeaders();
+        var params = new HttpParams().set('query', this.searchquery);
+
+        var searchterm = 'query=' + this.searchquery;
+
+        headers.append('Content-Type', 'application/X-www-form-urlencoded');
+
+        this.http_.post<TwitterApi>('http://localhost:3000/search', searchterm, { headers: headers, params: params }).subscribe((res) => {
+            this.tweetsdata = res.data.statuses;
+            console.log(this.tweetsdata);
+        });
+    }
+
+    title = 'Politiknördarna';
 }
